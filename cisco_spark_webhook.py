2e554cbed4d31e2be2251855326c1263b47575eb
Paul Anholt <panholt@gmail.com>
2017-09-19 08:04:09 -0400

    dont fetch deletes or messages from self

--- cisco_spark_webhook.py
+++ cisco_spark_webhook.py
@@ -16,3 +16,19 @@ class SparkWebhook(BotPlugin):
     @botcmd
     def webhook_stats(self, msg, *args):
         return 'Stats and stuff'
+
+    def process_stats(self, request):
+        try:
+            data = request['data']
+        except KeyError:
+            self.log.warning('Invalid data received on webhook')
+        if data['resource'] == 'messages':
+            pass
+        elif data['resource'] == 'memberships':
+            pass
+        elif data['resource'] == 'rooms':
+            pass
+        elif data['resource'] == 'teams':
+            pass
+        else:
+            self.log.warning('Unknown resource: %s', data['resource'])
